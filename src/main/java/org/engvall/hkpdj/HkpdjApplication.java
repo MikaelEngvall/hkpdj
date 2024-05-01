230b4ed450c6ca6acc67417c5691200b7cd9c1af
MikaelEngvall <mikael@engvall.org>
2024-05-01 14:34:21 +0200

    Initial commit

---
+++ src/main/java/org/engvall/hkpdj/HkpdjApplication.java
@@ -0,0 +1,13 @@
+package org.engvall.hkpdj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkpdjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HkpdjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/engvall/hkpdj/HkpdjApplicationTests.java
@@ -0,0 +1,13 @@
+package org.engvall.hkpdj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkpdjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
